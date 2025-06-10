107f36cb1b866abc885853a55dd31bc01400c743
Adila Zahra <adilazsandrina@gmail.com>
2025-06-10 21:35:44 +0700

    Penambahan asset dan recycler view untuk history

--- app/src/main/java/com/example/mobile_programming/MainActivity.java
+++ app/src/main/java/com/example/mobile_programming/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.mobile_programming;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,7 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        Intent inten = new Intent(this, HistoryActivity.class);
+        startActivity(inten);
     }
 }
